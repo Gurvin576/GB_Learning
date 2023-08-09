6c51c40a80dc2e6ff4fce96fdf6cc379e1387876
Dmitry <dmitry.basin@yandex.ru>
2023-08-09 19:48:04 +0300

    dotike

--- Example/Example002_HelloUser/Program.cs
+++ Example/Example002_HelloUser/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter Your Name: ");
+
+string name = Console.ReadLine();
+
+Console.WriteLine("Enter Your Age: ");
+
+int age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("You are " + name + " and you are " + age + " years old.");
